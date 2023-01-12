47d5d714777a62036de3e68e00ad9bb57b687c51
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-01-12 13:27:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,31 @@ import React from 'react'
 import '../styles/App.css';
 
 class App extends React.Component {
-  constructor(props){
-  super(props);
-      this.handleClick = this.handleClick.bind(this);
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "john Doe",
+      age: 34,
+      enrollmentNo: "12345678",
+      // year: 1964
+    };
+    this.handleClick = this.handleClick.bind(this);
+
   }
-  handleClick(){
-      let a =document.getElementById("age").innerHTML;
-      document.getElementById("age").innerHTML+1;
+  handleClick() {
+    document.getElementById("age").innerHTML = parseInt(document.getElementById("age").innerHTML) + 1;
+
   }
-  render (){
-  return {
-    <div>
-       Hello my name is <h1> {this.props.name}</h1> and I am <span id="age"> {this.props.age} </span> years old and my enrollment no is <p>{this.props.enrollmentNo} </p>
-         <button onclick={this.handleClick}>Increase Age</button>
-    </div>
+  render() {
+    return (
+      <>
+        <p>
+          Hello my name is <h1> {this.state.name}</h1> and I am <span id="age"> {this.state.age} </span> years old and my enrollment no is {this.state.enrollmentNo} </p>
+        <button onClick={this.handleClick}>Increase Age</button>
+      </>
+    );
   }
-  }
-  
+
 }
 
 
